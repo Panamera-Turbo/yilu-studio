de8d9e42667dddc80b71c3f24a043629866f6f87
Panamera-Turbo <1156742456@qq.com>
2021-01-28 15:00:14 +0800

    demo

--- free/vue-selflearn/day07/myVueDemo01/src/App.vue
+++ free/vue-selflearn/day07/myVueDemo01/src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <app-header></app-header>
+    <users></users>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Header from './components/header'
+import Footer from './components/footer'
+import Users from './components/Users'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      title:"This is my first vue doemo!"
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    "app-header" : Header,
+    "app-footer" : Footer,
+    "users" : Users,
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ free/vue-selflearn/day07/myVueDemo01/src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{ title}}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name:'header',
+    data(){
+        return {
+            title:"my vue.js"
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    background: lightblue;
+    padding: 10px;
+}
+
+h1{
+    color: #222;
+    text-align: center;
+}
+</style>
